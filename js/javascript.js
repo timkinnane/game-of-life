abe41333280d01f96950b91bba0d3ea00ec29920
sok213 <sophanarithsok@gmail.com>
2016-07-14 09:37:45 -0700

    add ability to add cells to grid

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,5 @@
 $(function() {
-	
+
 	//React GameOfLife Class below.
 	var GameOfLife = React.createClass({
 		getInitialState: function() {
@@ -7,16 +7,21 @@ $(function() {
 		},
 
 		render: function() {
-			var idNumber = 1500;
+			var idNumber = 1500,
+					changeCell = this.changeCell;
+
+			//Function that creates 50 squares to be returned within a row element.
 			var createSquares = function() {
 				var squares = [];
 				for(var i=50; i>0; i--) {
-					squares.push(<div className='square' key={idNumber} id={idNumber}></div>);
+					squares.push(<div className='square dead' key={idNumber} onClick={changeCell.bind(this, idNumber)} id={idNumber} ></div>);
 					idNumber--;
 				}
 				return squares;
 			};
 
+			//Function that creates 30 rows and calls upon the createSquare 
+			//function within each row element.
 			var createRows = function() {
 				var rows = [];
 				for(var i=30; i>0; i--) {
@@ -25,6 +30,17 @@ $(function() {
 				return rows;
 			};
 			return <div>{createRows()}</div>;
+		},
+
+		changeCell: function(squareId) {
+			var clickedSquare = squareId.toString();
+			if( $("#" + clickedSquare).hasClass('dead') ) {
+				$("#" + clickedSquare).removeClass('dead');
+				$("#" + clickedSquare).addClass('alive');
+			} else {
+				$("#" + clickedSquare).removeClass('alive');
+				$("#" + clickedSquare).addClass('dead');
+			}
 		}
 	});
 
