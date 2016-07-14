14a7866a8b94d42a2f218db826b6bb2e00e23a06
sok213 <sophanarithsok@gmail.com>
2016-07-14 08:14:08 -0700

    Remove unnecessary JavaScript code

--- js/javascript.js
+++ js/javascript.js
@@ -1,27 +1,5 @@
 $(function() {
-	console.log("javascript loaded successfully");
-	var idNumber = 1500,
-			squareRows = [];
-
-	function genDivElements() {
-		//This loop will append a 50x30 grid with 1500 squares each having an id 
-		//number, in numerical order, from descending order.
-		for(var i = 30; i > 0; i--) {
-			var divSquares = [];
-			//for every 50 div elements, put the elements between a
-			//<div class="row"> tag and append it to <div id="grid"> within the
-			//HTML document.
-			for(var squares=50; squares > 0; squares--) {
-				divSquares.push("<div class='square' id='"+idNumber+"'></div>");
-				idNumber--;
-			}
-			divSquares = "<div class='row'>"+divSquares.join('')+"</div>";
-			squareRows.push(divSquares);
-		}
-	}	
-
-	genDivElements();
-
+	
 	//React GameOfLife Class below.
 	var GameOfLife = React.createClass({
 		getInitialState: function() {
@@ -50,5 +28,5 @@ $(function() {
 		}
 	});
 
-	ReactDOM.render(<GameOfLife squares={squareRows}/>, document.getElementById('grid'));
+	ReactDOM.render(<GameOfLife />, document.getElementById('grid'));
 });
