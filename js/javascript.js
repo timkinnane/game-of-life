9f26c9c206d2f13936d48db3ccae2edc288daeb3
sok213 <sophanarithsok@gmail.com>
2016-07-15 00:49:16 -0700

    add generation count and contribute to  algorithm

--- js/javascript.js
+++ js/javascript.js
@@ -3,7 +3,10 @@ $(function() {
 	//React GameOfLife Class below.
 	var GameOfLife = React.createClass({
 		getInitialState: function() {
-			return { squareElements: this.props.squares };
+			return { 
+				squareElements: this.props.squares,
+				generationCount: 0
+			};
 		},
 
 		render: function() {
@@ -15,7 +18,7 @@ $(function() {
 			var createSquares = function() {
 				var squares = [];
 				for(var i=50; i>0; i--) {
-					squares.push(<div className='square dead' key={idNumber} onClick={changeCell.bind(this, idNumber)} id={idNumber} ></div>);
+					squares.push(<div className='square dead' key={idNumber} onClick={changeCell.bind(null, idNumber)} id={idNumber} ></div>);
 					idNumber--;
 				}
 				return squares;
@@ -34,7 +37,7 @@ $(function() {
 				<div>
 					<h2 className="title">Conway's Game of Life</h2>
 					<div className="control-buttons row col-md-12">
-		  			<button className="btn-success">Run</button>
+		  			<button className="btn-success" onClick={this.runGame}>Run</button>
 		  			<button className="btn-info">Pause</button>
 		  			<button className="btn-danger">Clear</button>
 		  			<a id="generations">Generations: </a>
@@ -54,6 +57,54 @@ $(function() {
 			} else {
 				$("#" + clickedSquare).removeClass('alive');
 				$("#" + clickedSquare).addClass('dead');
+			}
+		},
+
+		//Function that runs the game.
+		runGame: function() {
+			this.state.generationCount++;
+			$('#generations').html('Generations: ' + this.state.generationCount);
+
+			//for loop that iterates through all square id's.
+			for(var id = 1500; id > 0; id--) {
+
+				//Declare current square div's and all neighboring squares.
+				var currentSq = $("#" + id.toString()),
+						sqTop = $("#" + (id + 50).toString()),
+						sqBott = $("#" + (id - 50).toString()),
+						sqLeft = $("#" + (id + 1).toString()),
+						sqRight = $("#" + (id - 1).toString()),
+						sqTopLeft = $("#" + (id + 50 + 1).toString()),
+						sqTopRight = $("#" + (id + 50 -1).toString()),
+						sqBottLeft = $("#" + (id - 50 + 1).toString()),
+						sqBottRight = $("#" + (id - 50 -1).toString());
+
+				if(currentSq.hasClass('alive')) {
+
+					//if square has no alive neighbors.
+					if(sqTop.hasClass('dead') && sqBott.hasClass('dead') 
+							&& sqLeft.hasClass('dead') && sqRight.hasClass('dead') 
+							&& sqTopLeft.hasClass('dead') && sqTopRight.hasClass('dead') 
+							&& sqBottLeft.hasClass('dead') && sqBottRight.hasClass('dead')
+						) {
+						currentSq.removeClass('alive');
+						currentSq.addClass('dead');
+					}
+					//if top square is alive. 
+					else if
+						(sqTop.hasClass('alive') && sqBott.hasClass('dead') 
+							&& sqLeft.hasClass('dead') && sqRight.hasClass('dead') 
+							&& sqTopLeft.hasClass('dead') && sqTopRight.hasClass('dead') 
+							&& sqBottLeft.hasClass('dead') && sqBottRight.hasClass('dead')
+						) {
+						currentSq.removeClass('alive');
+						currentSq.addClass('dead');
+						var runGame = this.runGame;
+						setTimeout(function() {
+							runGame();
+						}, 300);
+					}
+				}
 			}
 		}
 	});
