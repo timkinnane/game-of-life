131a5634a7b4ff3469be2140760effead4f795a4
sok213 <sophanarithsok@gmail.com>
2016-07-15 09:13:31 -0700

    improve code and add notes

--- js/javascript.js
+++ js/javascript.js
@@ -2,23 +2,38 @@ $(function() {
 
 	//React GameOfLife Class below.
 	var GameOfLife = React.createClass({
+
+		//Initiates the generation count. 
 		getInitialState: function() {
 			return { 
-				squareElements: this.props.squares,
 				generationCount: 0
 			};
 		},
 
 		render: function() {
 			var idNumber = 1500,
-					changeCell = this.changeCell;
+					changeCell = this.changeCell,
+					squareObjects = [];
 
 			//Function that creates 50 squares to be returned 
 			//within a row element through the createRows function.
 			var createSquares = function() {
+
+				//Object contructor for making squares.
+				function squareObj(id) {
+					this.status = 'dead';
+					this.neighbors = 0;
+					this.id = id;
+				}
+
+				//Empty array that <div> square elements will be pushed into.
 				var squares = [];
+
+				//Create 50 new square objects and 50 <div> square elements.
 				for(var i=50; i>0; i--) {
-					squares.push(<div className='square dead' key={idNumber} onClick={changeCell.bind(null, idNumber)} id={idNumber} ></div>);
+					var newSquare = new squareObj(idNumber);
+					squareObjects.push(newSquare);
+					squares.push(<div className='square dead' key={idNumber} onClick={changeCell.bind(null, idNumber, squareObjects)} id={idNumber} ></div>);
 					idNumber--;
 				}
 				return squares;
@@ -37,9 +52,9 @@ $(function() {
 				<div>
 					<h2 className="title">Conway's Game of Life</h2>
 					<div className="control-buttons row col-md-12">
-		  			<button className="btn-success" onClick={this.runGame}>Run</button>
+		  			<button className="btn-success">Run</button>
 		  			<button className="btn-info">Pause</button>
-		  			<button className="btn-danger" onClick={this.clearGame}>Clear</button>
+		  			<button className="btn-danger" onClick={this.clearGame.bind(null, squareObjects)}>Clear</button>
 		  			<a id="generations">Generations: </a>
 		  		</div>
 		  		<div id="grid">{createRows()}</div>
@@ -49,18 +64,26 @@ $(function() {
 
 		//Function that is used as an onClick event for each square
 		//to change the status of a clicked square from dead to alive or vice versa.
-		changeCell: function(squareId) {
+		changeCell: function(squareId, squareObjects) {
 			var clickedSquare = squareId.toString();
+
+			//Retrieves the object equivalent of clickedSquare from squareObjects array.
+			var squareObj = this.returnSquareObj(squareObjects, squareId);
+
+			//Toggles the 'dead' or 'alive' class and changes the object
+			//status attribute to match it.
 			if( $("#" + clickedSquare).hasClass('dead') ) {
 				$("#" + clickedSquare).removeClass('dead');
 				$("#" + clickedSquare).addClass('alive');
+				squareObj.status = 'alive';
 			} else {
 				$("#" + clickedSquare).removeClass('alive');
 				$("#" + clickedSquare).addClass('dead');
+				squareObj.status = 'dead';
 			}
 		},
 
-		clearGame: function() {
+		clearGame: function(squareObjects) {
 			//Resets the generation count back to 0.
 			this.setState({generationCount: 0});
 			$('#generations').html('Generations: 0');
@@ -72,75 +95,27 @@ $(function() {
 					$("#" + id.toString()).addClass('dead');
 				}
 			}
+
+			//Sets status attribute of all square objects to "dead".
+			squareObjects.filter(function(sqObj){
+				sqObj.status = "dead";
+			});
 		},
 
-		//Function that runs the game.
+		//Function that takes the squareId, then finds the
+		//square object within the squareObjects array that matches 
+		//the squareId, then returns it. 
+		returnSquareObj: function(squareObjects, squareId) {
+			function findSquareById(square) {
+				return square.id == squareId;
+			}
+			var foundSquare = squareObjects.find(findSquareById);
+			return foundSquare;
+		},
+
+		//Function that runs the game. Game of Life rules algorithm goes here.
 		runGame: function() {
 
-			//for loop that iterates through all square id's.
-			for(var id = 1500; id > 0; id--) {
-
-				//Declare current square div's and all neighboring squares.
-				var currentSq = $("#" + id.toString()),
-						sqTop = $("#" + (id + 50).toString()),
-						sqBott = $("#" + (id - 50).toString()),
-						sqLeft = $("#" + (id + 1).toString()),
-						sqRight = $("#" + (id - 1).toString()),
-						sqTopLeft = $("#" + (id + 50 + 1).toString()),
-						sqTopRight = $("#" + (id + 50 -1).toString()),
-						sqBottLeft = $("#" + (id - 50 + 1).toString()),
-						sqBottRight = $("#" + (id - 50 -1).toString());
-
-				if(currentSq.hasClass('alive')) {
-
-					//if square has no alive neighbors.
-					if(sqTop.hasClass('dead') && sqBott.hasClass('dead') 
-							&& sqLeft.hasClass('dead') && sqRight.hasClass('dead') 
-							&& sqTopLeft.hasClass('dead') && sqTopRight.hasClass('dead') 
-							&& sqBottLeft.hasClass('dead') && sqBottRight.hasClass('dead')
-						) {
-						currentSq.removeClass('alive');
-						currentSq.addClass('dead');
-
-						this.state.generationCount++;
-						$('#generations').html('Generations: ' + this.state.generationCount);
-					}
-					//if top square is alive and the others are dead.
-					else if
-						(sqTop.hasClass('alive') && sqBott.hasClass('dead') 
-							&& sqLeft.hasClass('dead') && sqRight.hasClass('dead') 
-							&& sqTopLeft.hasClass('dead') && sqTopRight.hasClass('dead') 
-							&& sqBottLeft.hasClass('dead') && sqBottRight.hasClass('dead')
-						) {	
-						currentSq.removeClass('alive');
-						currentSq.addClass('dead');
-						var runGame = this.runGame;
-						setTimeout(function() {
-							runGame();
-						}, 200);
-
-						this.state.generationCount++;
-						$('#generations').html('Generations: ' + this.state.generationCount);
-					} 
-					//if bottom square is alive and others are dead.
-					else if
-						(sqBott.hasClass('alive') && sqTop.hasClass('dead') 
-							&& sqLeft.hasClass('dead') && sqRight.hasClass('dead') 
-							&& sqTopLeft.hasClass('dead') && sqTopRight.hasClass('dead') 
-							&& sqBottLeft.hasClass('dead') && sqBottRight.hasClass('dead')
-					  ) {
-							currentSq.removeClass('alive');
-							currentSq.addClass('dead');
-							var runGame = this.runGame;
-							setTimeout(function() {
-								runGame();
-							}, 200);
-
-							this.state.generationCount++;
-							$('#generations').html('Generations: ' + this.state.generationCount);
-					}
-				}
-			}
 		}
 	});
 
