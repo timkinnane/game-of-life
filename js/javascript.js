1b33dcb595b16ce15e62e082f999952f05334ea4
sok213 <sophanarithsok@gmail.com>
2016-07-18 21:34:27 -0700

    improve alogrithm

--- js/javascript.js
+++ js/javascript.js
@@ -21,6 +21,7 @@ $(function() {
 				//Object contructor for making squares.
 				function squareObj(id) {
 					this.status = 'dead';
+					this.state = 'unchecked';
 					this.neighbors = 0;
 					this.id = id;
 				}
@@ -63,6 +64,29 @@ $(function() {
 		//Function that runs the Game of Life algorithm. 
 		runGame: function(squareObjects) {
 
+			function checkPositions(position) {
+				//If position and center exists.
+				if(position) {
+					//If position is alive, increment center square's neighbor attribute by 1.
+					if(position.status == 'alive') {
+						center.neighbors++;
+					}
+				}
+			}
+
+			//Function that takes all positions and decrements the neighbors by 1.
+			function decrementNeighbors(position) {
+				if(position.neighbors >= 1) {
+					position.neighbors--;
+				}
+			}
+
+			function incremateNeighbors(position) {
+				if(position) {
+					position.neighbors++;
+				}
+			}
+
 			//Iterates through all squareObjects. 
 			for(var i = 1500; i>=0; i--) {
 				//Define the 8 possible neighboring positions.
@@ -88,28 +112,45 @@ $(function() {
 																 bottomRight
 																];
 
-				function checkPositions(position) {
-					//If position and center exists.
-					if(position) {
-						//If position is alive, increment center square's neighbor attribute by 1.
-						if(position.status == 'alive') {
-							center.neighbors++;
-						}
-					}
-				}
-
 				//Iterates through all posible positions of current square and passes it to
 				//checkPositions function as a parameter.
 				possiblePositions.filter(pos => {
 					checkPositions(pos);
 				});
+			}
+			//End of for loop.
+
+			//Second for loop that determines if a cell dies, reproduces, or stands still.
+			for(var i = 1500; i>=0; i--) {
+				var center = this.returnSquareObj(squareObjects, i.toString()),
+						bottom = this.returnSquareObj(squareObjects, (i-50).toString()),
+						top = this.returnSquareObj(squareObjects, (i+50).toString()),
+						right = this.returnSquareObj(squareObjects, (i-1).toString()),
+						left = this.returnSquareObj(squareObjects, (i+1).toString()),
+						topLeft = this.returnSquareObj(squareObjects, (i+50+1).toString()),
+						topRight = this.returnSquareObj(squareObjects, (i+50-1).toString()),
+						bottomLeft = this.returnSquareObj(squareObjects, (i-50+1).toString()),
+						bottomRight = this.returnSquareObj(squareObjects, (i-50-1).toString()),
+						possiblePositions = [
+																 bottom,
+																 top,
+																 right,
+																 left,
+																 topLeft,
+																 topRight,
+																 bottomLeft,
+																 bottomRight
+																];
 
 				//Rules of reproduction, death, or standstill.
-				if(center) { 
+				if(center) {
 					//if square had less than two neighbors OR if square had more than 3 neighbors,
 					//change status to dead.
 					if(center.neighbors < 2 || center.neighbors > 3 && center.status == 'alive') {
+						//Sets status of current square to dead, and decrements all neighbors squares by 1.
 						center.status = 'dead';
+						center.neighbors = 0;
+						decrementNeighbors(possiblePositions);
 					
 						//Changes the div element from 'alive' to 'dead'.
 						$('#' + center.id.toString()).removeClass('alive');
@@ -121,12 +162,13 @@ $(function() {
 						//if square is dead and has 3 neighbors, it comes to life.
 					} else if(center.status == 'dead' && center.neighbors == 3) {
 						center.status = 'alive';
+
 						$('#' + center.id.toString()).removeClass('dead');
 						$('#' + center.id.toString()).addClass('alive');
 					}
 				}
 			}
-			//End of for loop.
+			//End of second for loop.
 		},
 
 		//Function that is used as an onClick event for each square to change the status of 
@@ -178,6 +220,7 @@ $(function() {
 			var foundSquare = squareObjects.find(findSquareById);
 			return foundSquare;
 		}
+	//End of GameOfLfe React module.
 	});
 
 	ReactDOM.render(<GameOfLife />, document.getElementById('gameOfLife'));
